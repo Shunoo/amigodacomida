446386aded24a1553a2f3af5d258110c63b285e4
Dário Silva <dario.silva@supplystack.com>
2024-05-31 15:34:24 +0100

    creation of the category dto and meal dto

--- be/src/main/java/amigodacomida/dto/category/CategoryDTO.java
+++ be/src/main/java/amigodacomida/dto/category/CategoryDTO.java
@@ -1,4 +1,6 @@
 package amigodacomida.dto.category;
 
 public class CategoryDTO {
+     String type;
+     Integer mealId;
 }
--- be/src/main/java/amigodacomida/dto/meal/MealDTO.java
+++ be/src/main/java/amigodacomida/dto/meal/MealDTO.java
@@ -1,4 +1,9 @@
 package amigodacomida.dto.meal;
 
 public class MealDTO {
+     String name;
+     String estimatedtime;
+     String image;
+     String video;
+     Integer categoryId;
 }
